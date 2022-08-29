f3c41e60e35d20e1d5a7f38efcda9ba0ddfdcfc6
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-08-29 10:31:33 +0600

    router set up

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,10 +1,24 @@
+const Post = require("../models/post")
 
 // Get Posts_____________________________
-exports.getPosts = (req, res) => {
-    res.status(200).json({message: "ok"})
+exports.getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find()
+        res.status(200).json({ posts })
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 }
 
 // Create Post_____________________________
 exports.createPost = (req, res) => {
+    const post = req.body
+    const newPost = new Post({ post })
+    try {
+        await newPost.save()
+        res.status(201).json({ message: "Your Post has been saved." })
 
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 }
